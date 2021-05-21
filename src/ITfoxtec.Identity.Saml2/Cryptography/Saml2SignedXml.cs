02d25fdee1dd72236c76d78a44e9456acf351f1f
Mathias Feitzinger <mathias.feitzinger@world-direct.at>
2021-05-21 08:43:48 +0200

    removed SignatureMethod and CanonicalizationMethod equal check during SignatureChecking

--- src/ITfoxtec.Identity.Saml2/Cryptography/Saml2SignedXml.cs
+++ src/ITfoxtec.Identity.Saml2/Cryptography/Saml2SignedXml.cs
@@ -4,16 +4,15 @@ using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography.Xml;
 using System.Xml;
 
-namespace ITfoxtec.Identity.Saml2.Cryptography
-{
-    public class Saml2SignedXml : SignedXml
-    {
+namespace ITfoxtec.Identity.Saml2.Cryptography {
+    public class Saml2SignedXml : SignedXml {
         public XmlElement Element { get; protected set; }
+
         public Saml2Signer Saml2Signer { get; protected set; }
+
         public string CanonicalizationMethod { get; protected set; }
 
-        public Saml2SignedXml(XmlElement element, X509Certificate2 certificate, string signatureAlgorithm, string canonicalizationMethod) : base(element)
-        {
+        public Saml2SignedXml(XmlElement element, X509Certificate2 certificate, string signatureAlgorithm, string canonicalizationMethod) : base(element) {
             if (certificate == null) throw new ArgumentNullException(nameof(certificate));
             if (signatureAlgorithm == null) throw new ArgumentNullException(nameof(signatureAlgorithm));
             if (canonicalizationMethod == null) throw new ArgumentNullException(nameof(canonicalizationMethod));
@@ -23,8 +22,7 @@ namespace ITfoxtec.Identity.Saml2.Cryptography
             Saml2Signer = new Saml2Signer(certificate, signatureAlgorithm);
         }
 
-        public void ComputeSignature(X509IncludeOption includeOption, string id)
-        {
+        public void ComputeSignature(X509IncludeOption includeOption, string id) {
             var reference = new Reference("#" + id);
             reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
             reference.DigestMethod = SignatureAlgorithm.DigestMethod(Saml2Signer.SignatureAlgorithm);
@@ -53,16 +51,7 @@ namespace ITfoxtec.Identity.Saml2.Cryptography
                 throw new InvalidSignatureException("XML signature reference do not refer to the root element.");
             }
 
-            var canonicalizationMethodValid = SignedInfo.CanonicalizationMethod == CanonicalizationMethod;
-            var signatureMethodValid = SignedInfo.SignatureMethod == Saml2Signer.SignatureAlgorithm;
-            if (!(canonicalizationMethodValid && signatureMethodValid))
-            {
-                return false;
-            }
-            else
-            {                        
-                return CheckSignature(Saml2Signer.Certificate.GetRSAPublicKey());
-            }
+            return CheckSignature(Saml2Signer.Certificate.GetRSAPublicKey());
         }
     }
 }
