e2cd9a724f39a7ed22c33f19f11103c44b411662
Monica <monica.stoica94@gmail.com>
2024-03-19 12:19:25 +0100

    Updating assertion type
    Use protocol instead of assertion namespace.

--- src/ITfoxtec.Identity.Saml2/Schemas/Scoping.cs
+++ src/ITfoxtec.Identity.Saml2/Schemas/Scoping.cs
@@ -31,7 +31,7 @@ namespace ITfoxtec.Identity.Saml2.Schemas
 
         public XElement ToXElement()
         {
-            var envelope = new XElement(Saml2Constants.AssertionNamespaceX + elementName);
+            var envelope = new XElement(Saml2Constants.ProtocolNamespaceNameX + elementName);
 
             envelope.Add(GetXContent());
 
@@ -40,7 +40,7 @@ namespace ITfoxtec.Identity.Saml2.Schemas
 
         protected virtual IEnumerable<XObject> GetXContent()
         {
-            yield return new XAttribute(Saml2Constants.AssertionNamespaceNameX, Saml2Constants.AssertionNamespaceX);
+            yield return new XAttribute(Saml2Constants.ProtocolNamespaceNameX, Saml2Constants.ProtocolNamespaceNameX);
 
             if (RequesterID != null)
             {
